bbd99e9347e651555edbdde8c1763c8d8d5d3a91
iamk3 <kenneth.l.adams.iii@gmail.com>
2024-06-04 17:53:43 -0400

    allow for variable addresses and ints

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # win_ip_updater
+A batch file to input a static IP on a Windows machine.
--- new_ip.bat
+++ new_ip.bat
@@ -1,21 +1,54 @@
 @echo off
-:: Set the IP info. Usage
-:: netsh interface ipv4 set addres name="<interface_name>" static <IP_address> <Subnet_mask> <Default_gateway>
-netsh interface ipv4 set address name="DO NOT MODIFY" static 10.251.100.202 255.255.255.0 10.251.100.220
-:: Set DNS info. Usage
-:: netsh interface ipv4 set dns name="<interface_name>" static <dns_ip_address>
-netsh interface ipv4 set dns name="DO NOT MODIFY" static 10.251.100.220
-:: These next commands display the new information as configured,
-:: print "NEW IP COMPLETE" to the terminal, and pause the script
-netsh interface ipv4 show addresses "DO NOT MODIFY"
-netsh interface ipv4 show config "DO NOT MODIFY"
+cls
+::Display list of interfaces on Machine
+netsh interface show interface
+pause
+:: Set the IP info
+set /p interface_name="Interface Name: "
+set /p ip_address="IP Address: "
+set /p subnet_mask="Subnet Mask: "
+set /p default_gateway="Default Gateway: "
+set /p dns_ip_1="DNS Address: "
+:: Set IP Address
+netsh interface ipv4 set address name="%interface_name%" static %ip_address% %subnet_mask% %default_gateway%
+:: Set DNS info
+netsh interface ipv4 set dns name="%interface_name%" static %dns_ip_1%
+:: Display new information
+netsh interface ipv4 show addresses %interface_name%
+netsh interface ipv4 show config %interface_name%
 echo NEW IP COMPLETE
 pause
-:: If the configured information is incorrect, break the
-echo If the above info is incorrect, type <ctrl> c to break the program.
+echo If the above info is incorrect, type ctrl+c to break the program.
 echo otherwise Press any Key to continue
 pause
-echo SHUTTING DOWN NOW - Press any Key
+cls
+echo Press "s" to shutdown
+echo Press "r" to reboot
+echo Press "c" to complete the program
+set /p end="Enter Selection: "
+if /i "%end%"=="s" (
+	goto SHUTDOWN
+) else if /i "%end%"=="r" (
+	goto REBOOT
+) else (
+    goto END
+)
+:: This command shuts down the Windows machine after a 20 second delay
+:SHUTDOWN
+echo SHUTTING DOWN NOW
 pause
-:: This command shuts down the Windows machine with a 5 second delay
-shutdown /s /t 5
+shutdown /s /t 20
+goto END
+
+:: This command reboots the Windows machine after a 20 second delay
+:REBOOT
+echo REBOOTING NOW
+pause
+shutdown /r /t 20
+goto END
+
+:: End the program
+:END
+echo Program completed. Exiting.
+pause
+exit /b
